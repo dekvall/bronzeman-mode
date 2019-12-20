7e0d2187442ea15692df27177d1f967c6613ff25
dekvall <dkvldev@gmail.com>
2019-12-20 16:31:28 +0100

    small plugin update

--- src/main/java/dekvall/bronzeman/BronzemanModePlugin.java
+++ src/main/java/dekvall/bronzeman/BronzemanModePlugin.java
@@ -1,3 +1,27 @@
+/*
+ * Copyright (c) 2019, dekvall <https://github.com/dekvall>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package dekvall.bronzeman;
 
 import com.google.common.base.Strings;
@@ -17,6 +41,7 @@ import net.runelite.api.GameState;
 import net.runelite.api.InventoryID;
 import net.runelite.api.Item;
 import net.runelite.api.Player;
+import net.runelite.api.events.ClientTick;
 import net.runelite.api.events.GameStateChanged;
 import net.runelite.api.events.GameTick;
 import net.runelite.api.events.ItemContainerChanged;
@@ -33,7 +58,8 @@ import static net.runelite.http.api.RuneLiteAPI.GSON;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Bronzeman Mode"
+	name = "Bronzeman Mode",
+	description = "Unlock items as you acquire them (by dekvall)"
 )
 public class BronzemanModePlugin extends Plugin
 {
@@ -69,6 +95,7 @@ public class BronzemanModePlugin extends Plugin
 	@Getter(AccessLevel.PACKAGE)
 	private boolean itemsRecentlyUnlocked;
 	private int ticksToLastUnlock;
+	private boolean inGeRegion;
 
 	@Override
 	protected void startUp() throws Exception
@@ -176,17 +203,22 @@ public class BronzemanModePlugin extends Plugin
 		ticksToLastUnlock += 1;
 
 		killSearchResults();
+
+		Player localPlayer = client.getLocalPlayer();
+		inGeRegion = localPlayer != null && localPlayer.getWorldLocation().getRegionID() == GE_REGION;
+	}
+
+	@Subscribe
+	public void onClientTick(ClientTick event)
+	{
+		if (inGeRegion)
+		{
+			killSearchResults();
+		}
 	}
 
 	void killSearchResults()
 	{
-		Player localPlayer = client.getLocalPlayer();
-
-		if (localPlayer == null || localPlayer.getWorldLocation().getRegionID() != GE_REGION)
-		{
-			return;
-		}
-
 		Widget grandExchangeSearchResults = client.getWidget(162, 53);
 
 		if (grandExchangeSearchResults == null)
@@ -194,7 +226,7 @@ public class BronzemanModePlugin extends Plugin
 			return;
 		}
 
-		Widget[] children = grandExchangeSearchResults.getChildren();
+		Widget[] children = grandExchangeSearchResults.getDynamicChildren();
 
 		if (children == null || children.length < 2)
 		{
